92dd0e35f6fe5dcc238138647db2381613c18662
K Sheenan <kaynesheenan+git@gmail.com>
2021-10-05 14:34:50 +0900

    added runMain()

--- my-wave-portal-deneb/scripts/run.js
+++ my-wave-portal-deneb/scripts/run.js
@@ -1,4 +1,23 @@
-const main = async() => {
+const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory("wavePortal");
     const waveContract = await waveContractFactory.deploy();
+    // await for contract to be mined
+    await waveContract.deployed()
+
+    // print local address
+    console.log("Contract deployed to -- ", waveContract.address)
+
 }
+
+// async run main file and catch errors
+const runMain = async() => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
